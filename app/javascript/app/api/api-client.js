efc3d9822c691d6f8348b23258fd822cef2f3981
Gabriel Hamdan <ogabrielhamdan@gmail.com>
2021-02-21 04:40:29 -0300

    api cleanup and seed strategy improvement

--- app/javascript/app/api/api-client.js
+++ app/javascript/app/api/api-client.js
@@ -1,7 +1,4 @@
 import axios from 'axios';
 
 const apiClient = axios.create();
-
-apiClient.interceptors.response.use(({ data }) => ({ data }));
-
 export default apiClient;
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,3 @@
 profiles = JSON.parse(File.open(Rails.root.join('db/data/data_profiles.json')).read)
-profiles.each do |profile|
-  Profile.create(profile)
-end
+Profile.create(profiles)
 
