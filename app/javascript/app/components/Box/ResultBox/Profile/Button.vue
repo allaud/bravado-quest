d51d324f416f7985c79bd17570157afe198f3996
Gabriel Hamdan <ogabrielhamdan@gmail.com>
2021-02-21 01:25:46 -0300

    added result box

---
+++ app/javascript/app/components/Box/ResultBox/Profile/Button.vue
@@ -0,0 +1,31 @@
+<template>
+<button>
+  <p>{{label}}</p>
+</button>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    label: String,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+button {
+  border: none;
+  background: none;
+  padding-left: 15px;
+
+  p {
+    font-weight: 500;
+    font-size: 14px;
+    line-height: 16px;
+    color: #009688;
+    text-transform: capitalize;
+    text-align: left;
+  }
+}
+</style>
